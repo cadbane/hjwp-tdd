ffdd35c3119cfca15e7e3aa8c82e339f4063a75d
cadbane <33034420+cadbane@users.noreply.github.com>
2017-11-14 21:17:31 +0100

    now functional_tests check possibility of adding something to todo lists

--- functional_tests.py
+++ functional_tests.py
@@ -14,7 +14,23 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('Lists', self.browser.title)
-        # self.fail('End of the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Lists', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Thinks to do'
+        )
+
+        inputbox.send_keys('Buy some milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy some milk' for row in rows)
+        )
 
 
 if __name__ == '__main__':
